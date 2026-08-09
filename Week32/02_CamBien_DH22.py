a93c4ae0ceb6c60b43d85af825bfca6def53c9a7
Thomas <huutho.cse@gmail.com>
2026-08-09 06:49:13 +0000

    Udp

--- Week32/02_CamBien_DH22.py
+++ Week32/02_CamBien_DH22.py
@@ -7,8 +7,8 @@ camBien_dht22 = dht.DHT22(Pin(4))
 while True:
     cambien_dht22.measure() # Bật cảm biến
 
-    nhietDo = sensor.temperature() # Lệnh lấy nhiệt độ
-    doAm = sensor.humidity() # Lệnh lấy độ ẩm
+    nhietDo = camBien_dht22.temperature() # Lệnh lấy nhiệt độ
+    doAm = camBien_dht22.humidity() # Lệnh lấy độ ẩm
 
     print("Nhiet do:", nhietDo, "C")
     print("Do am:", doAm, "%")
